69e1db3a652490547339a0c610a768c239bff4f8
Nusaibabi <162562762+Nusaibabi@users.noreply.github.com>
2024-09-18 07:14:19 +0000

    jobdetail123

--- apps.py
+++ apps.py
@@ -2,3 +2,21 @@ import streamlit as st
 import pandas as pd
 
 st.header('Job details')
+data= pd.read_csv('/workspaces/Bank_Details/mentornow/bank.csv')
+
+
+a= data['job'].unique()
+
+
+option= st.selectbox('Select the job',(a))
+st.write(option)
+jobs = data 
+
+df= data[data['job']== option]
+st.write(df)
+
+ave=df['age'].mean()
+
+st.write(round(ave))
+
+st.metric(label="Avarage Age", value="39", delta="10" )
